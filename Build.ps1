835a285a218291d5895d8e420a7b48e5ef196160
Sam Greenhalgh <sam@radicalresearch.co.uk>
2016-09-07 00:09:29 +0100

    Added build script

---
+++ Build.ps1
@@ -0,0 +1,12 @@
+if(Test-Path .\artifacts) {
+    Remove-Item .\artifacts -Force -Recurse
+}
+
+& dotnet restore
+
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+& dotnet test .\test -c Release
+
+& dotnet pack .\src -c Release -o .\artifacts --version-suffix=$revision
